d089d632a10b9b92299399fb4a76d2b0651037b5
Ben Shackman <Bshackman@protonmail.com>
2019-02-20 13:35:49 -0500

    Actual proper commit for logging stuff

--- Assets/Scripts/InformationLogging/LoggingScript.cs
+++ Assets/Scripts/InformationLogging/LoggingScript.cs
@@ -1,30 +1,8 @@
 ﻿using System.Collections;
+using System;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class AreaLog
-{
-    private string areaName;
-    private int fails;
-    private int attemptTimeSeconds;
-
-    public AreaLog(string name)
-    {
-        areaName = name;
-        fails = 0;
-        attemptTimeSeconds = 0;
-    }
-
-    public void AddFail()
-    {
-        fails += 1;
-    }
-
-    public string GetName()
-    {
-        return areaName;
-    }
-}
 
 /// <summary>
 /// Script to output events from the playtest.
@@ -32,6 +10,35 @@ public class AreaLog
 public class LoggingScript : MonoBehaviour {
 
     public static LoggingScript LS;
+
+    public class AreaLog
+    {
+        private string areaName;
+        private int fails;
+        private int attemptTimeSeconds;
+
+        public AreaLog(string name)
+        {
+            areaName = name;
+            fails = 1;
+            attemptTimeSeconds = 0;
+        }
+
+        public void AddFail()
+        {
+            fails += 1;
+        }
+
+        public string GetName()
+        {
+            return areaName;
+        }
+
+        public override string ToString()
+        {
+            return "Area Name: " + areaName + " Fails: " + fails;
+        }
+    }
 
     List<AreaLog> areaLogs = new List<AreaLog>();
 
@@ -50,19 +57,34 @@ public class LoggingScript : MonoBehaviour {
         {
             areaLogs[logIndex].AddFail();
         }
-        else
+        else //Should never get called but just to be sure
         {
             areaLogs.Add(new AreaLog(name));
+            areaLogs[areaLogs.Count - 1].AddFail();
         }
     }
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void BuildLog(string name)
+    {
+        areaLogs.Add(new AreaLog(name));
+    }
+
+    private void OnApplicationQuit()
+    {
+        Debug.Log(DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss"));
+        string infoName = "UserLog: " + DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss");
+        string path = "Assets/Scripts/InformationLogging/Output.txt";
+
+        StreamWriter stream = new StreamWriter(path, true);
+        stream.WriteLine(infoName);
+        
+        foreach(AreaLog log in areaLogs)
+        {
+            stream.WriteLine(log.ToString());
+        }
+        stream.WriteLine("");
+        stream.WriteLine("");
+
+        stream.Close();
+    }
 }
