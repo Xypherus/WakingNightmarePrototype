567ea483f59e1602438ff20dd94d614fa65edb17
Payton Musco <pjmusco.2017@mymail.becker.edu>
2019-02-25 16:48:11 -0500

    Attempted to fix some Bugs
    Found some more

---
+++ Assets/Scripts/KillScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class KillScript : MonoBehaviour {
+    private Scene Currentscene;
+	// Use this for initialization
+	void Start () {
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player Has Triggered");
+        if (other.gameObject.tag == "Player")
+        {
+            Currentscene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(Currentscene.name);
+
+        }
+    }
+}
