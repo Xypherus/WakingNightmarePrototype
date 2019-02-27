04c449a757f47a06b9d88e68f59adb841f2cecf4
Ben Shackman <bshackman@protonmail.com>
2019-02-27 16:01:02 -0500

    Fear Stuff part 2

--- Assets/Scripts/PlayerFearController.cs
+++ Assets/Scripts/PlayerFearController.cs
@@ -17,11 +17,33 @@ public class PlayerFearController : MonoBehaviour {
     [Tooltip("Only visable for testing purposes, should never be manualy changed. Represents the player's current fear.")]
     public int currentFear;
 
+    /// <summary>
+    /// The range at which the player checks for enemies
+    /// </summary>
+    [Tooltip("The range at which the player checks for enemies")]
+    public float fearRange;
+
+    /// <summary>
+    /// How frequently the fear tick checks. 
+    /// </summary>
+    [Tooltip("How frequently the fear tick checks.")]
+    public float fearTickTime = .5f;
+
     #endregion
+
+    private Collider2D[] inRange;
+    private LayerMask enemyMask;
 
     private void Start()
     {
         currentFear = 0;
+        enemyMask = LayerMask.GetMask("Enemy");
+        InvokeRepeating("FearTicker", 1, fearTickTime);
+    }
+
+    private void FearTicker()
+    {
+        inRange = Physics2D.OverlapCircleAll(transform.position, fearRange, enemyMask);
     }
 
     public void FearUpdater()
