bb8c4d59292df1815931823fbf7e2040463e80b0
Payton Musco <pjmusco.2017@mymail.becker.edu>
2019-03-25 22:02:06 -0400

    Began working on triggers and door framework

---
+++ Assets/Scripts/Door_Trap_Switch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door_Trap_Switch : MonoBehaviour{
+    triggerscript trigger;
+    private Animator open;
+    public string switch_key = "v";
+
+    private void Start()
+    {
+        gameObject.GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if(trigger.triggered == true && gameObject.CompareTag("Door"))
+        {
+            open.SetBool("isopen", true);
+        }
+        if(trigger.triggered == true && gameObject.CompareTag("Trap"))
+        {
+            gameObject.SetActive(true);
+        }
+        if (trigger.triggered == true && gameObject.CompareTag("Switch"))
+        {
+            if (Input.GetKeyDown(switch_key))
+            {
+                open.SetBool("isopen", true)
+            }
+        }
+        else
+        {
+            open.SetBool("isopen", false);
+        }
+    }
+}
