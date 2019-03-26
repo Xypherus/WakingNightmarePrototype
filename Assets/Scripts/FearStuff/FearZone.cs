77c05f3913af258692eef548c11e5dd3b7d0f338
Ben Shackman <Bshackman@protonmail.com>
2019-03-26 18:51:43 -0400

    Added support for fear zones
    Also included empty death function. Currently has no function but does get called when the player should die.

---
+++ Assets/Scripts/FearStuff/FearZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class FearZone : MonoBehaviour {
+
+    /// <summary>
+    /// The type of fear which this zone applies
+    /// </summary>
+    [Tooltip("The type of fear which this zone applies")]
+    public FearTypes fearType;
+
+    /// <summary>
+    /// The ammount of fear which this zone applies per feartick
+    /// </summary>
+    [Tooltip("The ammount of fear which this zone applies per feartick")]
+    public int fearApplied;
+    
+}
