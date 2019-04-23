2f77df2099b5c39777a9f6ff3b7c28e322596077
Ben Shackman <Bshackman@protonmail.com>
2019-04-23 16:28:55 -0400

    Main Menu Stuff

---
+++ Assets/Scripts/UIStuff/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    /// <summary>
+    /// The number of the first level in the build settings
+    /// </summary>
+    [Tooltip("The number of the first level in the build settings")]
+    public int firstLevelValue = 1;
+
+    /// <summary>
+    /// The number of the credits level in the build settings
+    /// </summary>
+    [Tooltip("The number of the credits level in the build settings")]
+    public int creditsLevelValue = 2;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	void StartGame()
+    {
+        GameManager.GM.LoadLevel(firstLevelValue);
+    }
+
+    void LoadCreditsMenu()
+    {
+        GameManager.GM.LoadLevel(creditsLevelValue);
+    }
+}
